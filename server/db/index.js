ff52b95d0c9a85909b29d9f85b893143fecbce35
Michelle-Ward <Mward352@gmail.com>
2020-12-17 17:09:41 -0800

    change export

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,11 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user 'root', no password,
 // and to the database 'chat'.
+//module.export {}
 
 var con = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
+  user: 'student',
   database: 'chat',
   password: 'student'
 });
