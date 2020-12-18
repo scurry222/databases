c95969586aa924813bfe39594345d31ea83cda4e
Michelle-Ward <Mward352@gmail.com>
2020-12-17 17:14:30 -0800

    fix controllers to pass on requests

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -2,12 +2,26 @@ var models = require('../models');
 
 module.exports = {
   get: function (req, res) {
-
+    models.messages.getAll((err, data) => {
+      if (err) {
+        res.status(500);
+        throw new Error('Failed to get all messages');
+      } else {
+        res.status(200);
+        res.send(data);
+      }
+    });
   }, // a function which handles a get request for all messages
+
   post: function (req, res) {
-    console.log(req, res);
-    res.send();
-    //have formatted message object
-    // call the model method create to put message obj in to the database
+    models.messages.create(req.body, (err) => {
+      if (err) {
+        res.status(500);
+        throw new Error('Failed to create message');
+      } else {
+        res.status(201);
+        res.send();
+      }
+    });
   } // a function which handles posting a message to the database
 };
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,26 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {},
-  post: function (req, res) {}
+  get: function (req, res) {
+    models.users.getAll((err, data) => {
+      if (err){
+        res.status(500);
+        throw new Error('Failed to get all users data');
+      } else {
+        res.status(200);
+        res.send();
+      }
+    });
+  },
+  post: function (req, res) {
+    models.users.create(req.body, (err) => {
+      if (err) {
+        res.status(500);
+        throw new Error('Failed to create user');
+      } else {
+        res.status(200);
+        res.send();
+      }
+    });
+  }
 };
