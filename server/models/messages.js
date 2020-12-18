935f14142a046627371feabcb71e1682839eb188
Michelle-Ward <Mward352@gmail.com>
2020-12-17 17:08:30 -0800

    fix for reference of db connection and queries

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,14 +1,16 @@
 var db = require('../db');
+
 //con is our variable for the created connection in db
 
 module.exports = {
   getAll: function (callback) {
     var queryString = 'SELECT messages.messagetext, users.username, rooms.roomname FROM messages INNER JOIN rooms ON rooms.id=messages.roomid INNER JOIN users ON users.id=messages.userid';
-    con.query(queryString, (err, result) => {
+    db.query(queryString, (err, result) => {
       if (err) {
         callback(err);
       } else {
-        callback(result);
+        console.log('the data result of select: ', result);
+        callback(null, result);
       }
     });
   }, // a function which produces all the messages
@@ -18,16 +20,15 @@ module.exports = {
   //pulls the created individual message from the database
   create: function (message, callback = ()=>{}) {
     //insert message obj into the messages table?
-    var queryString = `INSERT INTO messages (messagetext, timedate) VALUES (${message.text}, ${message.dateTime})`;
-    var qString2 = `INSERT INTO rooms (roomname) VALUES (${message.roomname})`;
+    var queryString = `INSERT INTO messages (messagetext, userid, roomname) VALUES ('${message.text}', SELECT id FROM users WHERE username = '${message.username}', SELECT id FROM rooms WHERE roomname = '${message.roomname}')`;
     //pose query to database for messages
-    con.query(queryString, (err) => {
+    db.query(queryString, (err) => {
       if (err) {
         callback(err);
         throw new Error('Failure to populate messages table');
       }
     });
-    con.query(qString2, (err) => {
+    db.query(qString2, (err) => {
       if (err) {
         callback(err);
       }
--- server/models/users.js
+++ server/models/users.js
@@ -3,19 +3,20 @@ var db = require('../db');
 module.exports = {
   getAll: function (callback) {
     var queryString = 'SELECT username FROM users';
-    con.query(queryString, (err, result) => {
+    db.query(queryString, (err, result) => {
       if (err) {
         res.status(500);
         throw new Error('Failure to get all users data from database');
       } else {
-        callback(result);
+        callback(null, result);
       }
     });
   },
   create: function (user, callback = () => {}) {
-    var queryString = `INSERT INTO users (users.username) VALUES (${user.username})`;
-    con.query(queryString, (err) => {
+    var queryString = `INSERT INTO users (username) VALUES ('${user.username}')`;
+    db.query(queryString, (err, results) => {
       if (err) {
+        console.log(err);
         callback(err);
       }
     });
